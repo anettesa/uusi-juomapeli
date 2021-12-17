900cbee773910c8d09a8131e88d00146394bc83a
Heikki Hellgren <heikki.hellgren@maxtech.fi>
2021-12-17 19:55:35 +0200

    fix: issues in javascript + reformat

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 // JavaScript source code
 //Get all the images
-const imageArray = [
-    "assets/Bodyshot.png",
-    "assets/Sääntö.png",
-    ];
-
+const imageArray = ["assets/Bodyshot.png", "assets/Sääntö.png"];
 
 const image = document.querySelector("img");
-const button = document.querySelector("button");
 
-window.onload = () => { shuffleArray(imageArray);
-    image.src = image.Array.shift(); 
-    }
+window.onload = () => {
+  shuffleArray(imageArray);
+  image.src = imageArray.shift();
+};
 
-button.addEventListener("click", () => suffleArray(imageArray),
-    image.src=imageArray.shift()
-    );
+image.addEventListener("click", () => {
+  image.src = imageArray.shift();
+});
 
-function shuffleArray(arrray) {
-    for (let i = array.length -1;1>0;i--){
-        const j =
-        Math.floor(Math.random()*(i+1));
-            [array[i], array[j]=array[j],array[i]];
-    }
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    console.log(i);
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
-
-    
