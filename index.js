0563b0be799d84a740718ed75efe57f0a8e92f5c
AnetteSa <salmela.anette@gmail.com>
2021-12-17 20:46:26 +0200

    v6
    start page added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // JavaScript source code
 //Get all the images
-const imageArray = ["assets/Bodyshot.png", "assets/Sääntö.png"];
+const imageArray = ["assets/1.png", "assets/2.png", "assets/3.png", "assets/4.png", "assets/5.png", 
+"assets/6.png", "assets/7.png", "assets/8.png", "assets/9.png", "assets/10.png", "assets/11.png", "assets/12.png", 
+"assets/13.png", "assets/14.png", "assets/15.png", "assets/16.png", "assets/17.png", "assets/18.png", 
+"assets/19.png", "assets/20.png", "assets/21.png", "assets/21.png", "assets/22.png", "assets/23.png",
+"assets/24.png", "assets/25.png", "assets/26.png",
+];
 
 const image = document.querySelector("img");
 
