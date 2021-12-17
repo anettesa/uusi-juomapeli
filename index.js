eb962522e68a7431f81929215623cc6838dca7fb
AnetteSa <salmela.anette@gmail.com>
2021-12-17 19:30:21 +0200

    v3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // JavaScript source code
 //Get all the images
-const imageArray = ["assets/Bodyshot.png", "assets/Sääntö.png"];
+const imageArray = ["assets/Bodyshot.png", "assets/Sääntö.png", "assets\Cheers, kaikki juo!.png", "assets\Drinkkimestari.png"];
 
 const image = document.querySelector("img");
 const button = document.querySelector("button");
@@ -13,7 +13,7 @@ window.onload = () => {
 image.addEventListener( "click", () => {image.src = imageArray.shift()}
 );
 
-function shuffleArray(arrray) {
+function shuffleArray(array) {
   for (let i = array.length - 1; 1 > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], (array[j] = array[j]), array[i]];
