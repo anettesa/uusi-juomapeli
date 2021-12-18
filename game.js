bb2cd303c48365971e227558b76bc09ae4e4fb6c
Heikki Hellgren <heikki.hellgren@maxtech.fi>
2021-12-18 14:52:18 +0200

    feat: allow selecting game mode
    also few other updates

--- game.js
+++ game.js
@@ -1,21 +1,13 @@
 // JavaScript source code
-//Get all the images
-const imageArray = ["assets/1.png", "assets/2.png", "assets/3.png", "assets/4.png", "assets/5.png", 
-"assets/6.png", "assets/7.png", "assets/8.png", "assets/9.png", "assets/10.png", "assets/11.png", "assets/12.png", 
-"assets/13.png", "assets/14.png", "assets/15.png", "assets/16.png", "assets/17.png", "assets/18.png", 
-"assets/19.png", "assets/20.png", "assets/21.png", "assets/21.png", "assets/22.png", "assets/23.png",
-"assets/24.png", "assets/25.png", "assets/26.png", "assets/27.png", "assets/28.png", "assets/29.png", "assets/30.png", "assets/31.png",
-"assets/32.png", "assets/33.png", "assets/34.png", "assets/35.png", "assets/36.png", "assets/38.png",
-"assets/39.png", "assets/40.png", "assets/41.png", "assets/42.png", "assets/43.png", "assets/44.png", "assets/45.png",
-"assets/46.png", "assets/47.png", "assets/48.png", "assets/49.png", "assets/50.png", "assets/51.png", "assets/52.png",
-"assets/53.png", "assets/54.png", "assets/55.png", "assets/56.png", "assets/57.png", "assets/58.png",
-];
 const NUMBER_OF_CARDS = 57;
 // Tehdään array jossa on korttien numerot 1-NUMBER_OF_CARDS
 const cardNumbers = [...Array(NUMBER_OF_CARDS).keys()].map((x) => x + 1);
 
 // Kopioidaan uuteen vektoriin
 let imageNumbers = [...cardNumbers];
+
+// Tässä mode mitä pelataan, tällä voi tehdä sitten muutoksia
+const GAME_MODE = new URLSearchParams(window.location.search).get("mode");
 
 // Tarvittavat komponentit, haetaan ID:llä
 const card = document.getElementById("card");
@@ -25,11 +17,18 @@ const gameEnd = document.getElementById("gameEnd");
 const startAgain = document.getElementById("startAgain");
 let frontShown = true;
 
+// Tämä funktio lataa kuvat ennen niiden käyttöä selaimen muistiin
+function preloadImage(idx) {
+  const img = new Image();
+  img.src = `assets/${idx}.png`;
+}
+
 function startGame() {
   // Piilotetaan lopputekstit ja napit
   gameEnd.style.display = "none";
   // Sekoitetaan pakka
   shuffleArray(imageNumbers);
+  preloadImage(imageNumbers[0]);
   // Resetoidaan korttien tila
   card.classList.remove("flipped");
   frontShown = true;
@@ -52,6 +51,7 @@ startAgain.addEventListener("click", () => {
 card.addEventListener("click", () => {
   // Jos ei yhtään numeroa jäljellä, näytä lopputeksti ja poista kuvat
   if (imageNumbers.length === 0) {
+    card.classList.toggle("flipped");
     gameEnd.style.display = "block";
     back.style.removeProperty("background-image");
     front.style.removeProperty("background-image");
@@ -64,6 +64,10 @@ card.addEventListener("click", () => {
     // muuten lataa etupuolelle uusi kortti
   } else {
     front.style.backgroundImage = `url(assets/${imageNumbers.shift()}.png)`;
+  }
+
+  if (imageNumbers.length > 0) {
+    preloadImage(imageNumbers[0]);
   }
   // Ja nyt käännä kortti
   card.classList.toggle("flipped");
