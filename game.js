7572c89900f0c02f36bb06de8c975a3471297588
AnetteSa <salmela.anette@gmail.com>
2021-12-19 16:21:29 +0200

    new cards + few changes

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 // JavaScript source code
-const NUMBER_OF_CARDS = 57;
+const NUMBER_OF_CARDS = 89;
 // Tehdään array jossa on korttien numerot 1-NUMBER_OF_CARDS
 let cardNumbers = [...Array(NUMBER_OF_CARDS).keys()].map((x) => x + 1);
 
@@ -10,11 +10,11 @@ let GAME_MODE_CARDS = [];
 switch (GAME_MODE) {
   case "relaxed":
     // Tässä määritellään mitä kortteja käytetään hardcore modessa
-    GAME_MODE_CARDS = [2, 3, 7, 8, 9, 10, 11, 14, 16, 17, 19, 20, 22, 23, 24, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50, 52, 53, 54, 55, 57];
+    GAME_MODE_CARDS = [2, 3, 7, 8, 9, 10, 11, 14, 16, 17, 19, 20, 22, 23, 24, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50, 52, 53, 54, 55, 57, 58, 60, 64, 65, 73, 75, 76, 79, 80, 81, 82, 83, 86];
     break;
   case "hardcore":
     // Tässä määritellään mitä kortteja käytetään rennossa modessa
-    GAME_MODE_CARDS = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 25, 26, 27, 28, 29, 30, 31, 34, 35, 36, 37, 40, 41, 44, 45, 48, 49, 51, 53, 54, 55, 56];
+    GAME_MODE_CARDS = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 25, 26, 27, 28, 29, 30, 31, 34, 35, 36, 37, 40, 41, 44, 45, 48, 49, 51, 53, 54, 55, 56, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 83, 84, 85, 87, 88, 89];
     break;
   // Lisää pelimuotoja voi tehdä kun lisää eri caseja
   case "normal":
