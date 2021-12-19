2b2c4decf6ace2ee7ba18eba3eefb5cc73487728
Heikki Hellgren <heikki.hellgren@maxtech.fi>
2021-12-19 17:02:02 +0200

    improve mode cards selection a bit

--- game.js
+++ game.js
@@ -5,27 +5,30 @@ let cardNumbers = [...Array(NUMBER_OF_CARDS).keys()].map((x) => x + 1);
 
 // Tässä mode mitä pelataan, tällä voi tehdä sitten muutoksia
 const GAME_MODE = new URLSearchParams(window.location.search).get("mode");
-let GAME_MODE_CARDS = [];
 
 switch (GAME_MODE) {
   case "relaxed":
     // Tässä määritellään mitä kortteja käytetään hardcore modessa
-    GAME_MODE_CARDS = [2, 3, 7, 8, 9, 10, 11, 14, 16, 17, 19, 20, 22, 23, 24, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50, 52, 53, 54, 55, 57, 58, 60, 64, 65, 73, 75, 76, 79, 80, 81, 82, 83, 86];
+    cardNumbers = [
+      2, 3, 7, 8, 9, 10, 11, 14, 16, 17, 19, 20, 22, 23, 24, 27, 28, 29, 30, 31,
+      32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50,
+      52, 53, 54, 55, 57, 58, 60, 64, 65, 73, 75, 76, 79, 80, 81, 82, 83, 86,
+    ];
     break;
   case "hardcore":
     // Tässä määritellään mitä kortteja käytetään rennossa modessa
-    GAME_MODE_CARDS = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 25, 26, 27, 28, 29, 30, 31, 34, 35, 36, 37, 40, 41, 44, 45, 48, 49, 51, 53, 54, 55, 56, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 83, 84, 85, 87, 88, 89];
+    cardNumbers = [
+      1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      23, 25, 26, 27, 28, 29, 30, 31, 34, 35, 36, 37, 40, 41, 44, 45, 48, 49,
+      51, 53, 54, 55, 56, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70, 71,
+      72, 73, 74, 75, 76, 77, 78, 79, 80, 83, 84, 85, 87, 88, 89,
+    ];
     break;
   // Lisää pelimuotoja voi tehdä kun lisää eri caseja
   case "normal":
   default:
     // Normaalisti oteta mitään pois
     break;
-}
-
-// Otetaan pois kortit jotka ei ole käytössä pelimuodossa
-if (GAME_MODE_CARDS.length > 0) {
-  cardNumbers = cardNumbers.filter((id) => GAME_MODE_CARDS.includes(id));
 }
 
 // Kopioidaan uuteen vektoriin
