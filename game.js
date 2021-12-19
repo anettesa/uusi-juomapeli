c455a5d3eedac7ed1d80100bf385882865f6f26e
AnetteSa <salmela.anette@gmail.com>
2021-12-19 14:34:10 +0200

    korttien säätöä

--- game.js
+++ game.js
@@ -8,13 +8,13 @@ const GAME_MODE = new URLSearchParams(window.location.search).get("mode");
 let GAME_MODE_CARDS = [];
 
 switch (GAME_MODE) {
-  case "hardcore":
-    // Tässä määritellään mitä kortteja käytetään hardcore modessa
-    GAME_MODE_CARDS = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 25, 26, 27, 28, 29, 30, 31, 34, 35, 36, 37, 40, 41, 44, 45, 48, 49, 51, 53, 54, 55, 56];
-    break;
   case "relaxed":
-    // Tässä määritellään mitä kortteja käytetään rennossa modessa
+    // Tässä määritellään mitä kortteja käytetään hardcore modessa
     GAME_MODE_CARDS = [2, 3, 7, 8, 9, 10, 11, 14, 16, 17, 19, 20, 22, 23, 24, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50, 52, 53, 54, 55, 57];
+    break;
+  case "hardcore":
+    // Tässä määritellään mitä kortteja käytetään rennossa modessa
+    GAME_MODE_CARDS = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 25, 26, 27, 28, 29, 30, 31, 34, 35, 36, 37, 40, 41, 44, 45, 48, 49, 51, 53, 54, 55, 56];
     break;
   // Lisää pelimuotoja voi tehdä kun lisää eri caseja
   case "normal":
