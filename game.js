d22ee715b0a1ce09a52251474ac2bf7b809c218f
Heikki Hellgren <heikki.hellgren@maxtech.fi>
2021-12-21 20:22:13 +0200

    play sound when card changes

--- game.js
+++ game.js
@@ -10,7 +10,8 @@ let timeForNewCard = 10;
 let roundsForTimer = 10;
 // Tehdään array jossa on korttien numerot 1-NUMBER_OF_CARDS
 let cardNumbers = [...Array(NUMBER_OF_CARDS).keys()].map((x) => x + 1);
-
+// Kääntöääni
+var flipSound = new Audio("assets/flip.mp3");
 // Tässä mode mitä pelataan, tällä voi tehdä sitten muutoksia
 const GAME_MODE = new URLSearchParams(window.location.search).get("mode");
 
@@ -21,7 +22,7 @@ switch (GAME_MODE) {
       2, 3, 7, 8, 9, 10, 11, 14, 16, 17, 19, 20, 22, 23, 24, 27, 28, 29, 30, 31,
       32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50,
       52, 53, 54, 55, 57, 58, 60, 64, 65, 73, 75, 76, 79, 80, 81, 82, 83, 86,
-       91, 92, 94, 95, 110, 113, 114, 115, 116, 117, 118
+      91, 92, 94, 95, 110, 113, 114, 115, 116, 117, 118,
     ];
     break;
   case "extreme":
@@ -35,9 +36,9 @@ switch (GAME_MODE) {
       1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
       23, 25, 26, 27, 28, 29, 30, 31, 34, 35, 36, 37, 40, 41, 44, 45, 48, 49,
       51, 53, 54, 55, 56, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70, 71,
-      72, 73, 74, 75, 76, 77, 78, 79, 80, 83, 84, 85, 87, 88, 89, 90, 91, 92, 
-      93, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 
-      112, 113, 114, 115, 116, 117, 118
+      72, 73, 74, 75, 76, 77, 78, 79, 80, 83, 84, 85, 87, 88, 89, 90, 91, 92,
+      93, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 112,
+      113, 114, 115, 116, 117, 118,
     ];
     break;
   // Lisää pelimuotoja voi tehdä kun lisää eri caseja
@@ -87,6 +88,8 @@ function showNextCard() {
     shuffleArray(imageNumbers);
   }
 
+  // Soitetaan ääni
+  flipSound.play();
   // Jos etupuoli näkyvissä, lataa takapuolelle uusi kortti (hehe...)
   if (frontShown) {
     back.style.backgroundImage = `url(assets/${imageNumbers.shift()}.png)`;
